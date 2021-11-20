b8f656cf45555671168468c8ce8da60faff476dc
fab41420 <fabian.braun@aisec.fraunhofer.de>
2021-11-20 21:51:10 +0100

    fixed it

--- app/src/main/java/com/example/automato/PlantDetails.java
+++ app/src/main/java/com/example/automato/PlantDetails.java
@@ -7,23 +7,27 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PlantDetails extends AppCompatActivity {
-    Plant plant = (Plant) getIntent().getSerializableExtra("PLANT");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plant_details);
+        Plant plant = (Plant) getIntent().getSerializableExtra("PLANT");
 
         TextView plantName = findViewById(R.id.plantName);
         ImageView plantPicture = findViewById(R.id.plantPicture);
         TextView currWaterlvl = findViewById(R.id.curr_water);
         TextView currTemp = findViewById(R.id.curr_temp);
         TextView currlight = findViewById(R.id.currlight);
+        TextView message = findViewById(R.id.message);
 
         plantName.setText(plant.getName());
         plantPicture.setImageResource(plant.getPicture());
         currWaterlvl.setText(plant.getCurrentSoilHumidity() + "%");
         currTemp.setText(plant.getCurrentTemperature() + "°C");
-        currlight.setText(plant.getCurrentLightLevel()+"%");
+        currlight.setText(plant.getCurrentLightLevel() + "%");
+        StringBuilder b = new StringBuilder();
+        plant.getDetailedMessage().stream().filter(s -> s != null).forEach(s -> b.append("\n").append(s));
+        message.setText(b.toString());
     }
 }
