4de2e9f860fb2548c7812ed77fd8a1d07c14fb24
pollyla <lassmann.pauline@gmail.com>
2021-11-20 17:48:40 +0100

    new activity

--- app/src/main/java/com/example/automato/MainActivity.java
+++ app/src/main/java/com/example/automato/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.automato;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<ExampleItem> mExampleList;
 
     private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
+    private ExampleAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
 
     private Button buttonInsert;
@@ -36,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         createExampleList();
         buildRecyclerView();
@@ -111,6 +111,16 @@ public class MainActivity extends AppCompatActivity {
 
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setAdapter(mAdapter);
+
+        mAdapter.setOnItemClickListener(new ExampleAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                Intent intent = new Intent(MainActivity.this, PlantDetails.class);
+                startActivity(intent);
+                //mExampleList.get(position).changeText1("test");
+                //mAdapter.notifyItemChanged(position);
+            }
+        });
 
 
 //_____________________________________________________
--- app/src/main/java/com/example/automato/PlantDetails.java
+++ app/src/main/java/com/example/automato/PlantDetails.java
@@ -7,7 +7,7 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PlantDetails extends AppCompatActivity {
-    Plant plant = new Plant("Tomate", "Tobi");
+    //Plant plant = new Plant("Tomate", "Tobi", true);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +19,9 @@ public class PlantDetails extends AppCompatActivity {
         TextView currWaterlvl = findViewById(R.id.curr_water);
         TextView currTemp = findViewById(R.id.curr_temp);
 
-        plantName.setText(plant.getName());
-        plantPicture.setImageResource(plant.getPicture());
-        currWaterlvl.setText(plant.getWaterlevel() + "%");
-        currTemp.setText(plant.getTemperature() + "°C");
+        //plantName.setText(plant.getName());
+        //plantPicture.setImageResource(plant.getPicture());
+        //currWaterlvl.setText(plant.getCurrentWaterLevel() + "%");
+        //currTemp.setText(plant.getCurrentTemperature() + "°C");
     }
 }
